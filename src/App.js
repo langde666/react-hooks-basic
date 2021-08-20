d044b5bc91e678ba8b467248b7821ee6ab2562be
Nghia Dang <vannghiadang79@gmail.com>
2021-08-20 11:15:17 +0700

    useEffect - pagination

--- src/App.js
+++ src/App.js
@@ -1,49 +1,71 @@
 import React, { useState, useEffect, } from 'react';
+import queryString from 'query-string';
 import './App.scss';
 import './components/ColorBox/ColorBox.scss';
 import ColorBox from './components/ColorBox/index';
 import TodoList from './components/TodoList/index';
 import TodoForm from './components/TodoForm/index';
 import PostList from './components/PostList/index';
+import Pagination from './components/Pagination/index';
 
 function App() {
-  const [todoList, setTodoList] = useState(
-    [
-      { id: 1, title: 'Doing what you like is freedom. Liking what you do is happiness.' },
-      { id: 2, title: 'I find my greatest pleasure, and so my reward, in the work that precedes what the world calls success.' },
-      { id: 3, title: 'The woman who can create her own job is the woman who will win fame and fortune.' },
-    ]
-  );
+  // const [todoList, setTodoList] = useState(
+  //   [
+  //     { id: 1, title: 'Doing what you like is freedom. Liking what you do is happiness.' },
+  //     { id: 2, title: 'I find my greatest pleasure, and so my reward, in the work that precedes what the world calls success.' },
+  //     { id: 3, title: 'The woman who can create her own job is the woman who will win fame and fortune.' },
+  //   ]
+  // );
 
   const [postList, setPostList] = useState([]);
+  const [pagination, setPagination] = useState({
+    _page: 1,
+    _limit: 10,
+    _totalRows: 1,
+  });
+  const [filters, setFilters] = useState({
+    _limit: 10,
+    _page: 1,
+  });
 
   useEffect(() => {
     async function fetchPostList() {
       try {
-        const requestUrl = 'http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1';
+        //npm i query-string
+        const paramsString = queryString.stringify(filters);
+        const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramsString}`;
         const response = await fetch(requestUrl);
         const responseJSON = await response.json();
         // console.log({responseJSON});
 
-        const { data } = responseJSON;
+        const { data, pagination } = responseJSON;
         setPostList(data);
+        setPagination(pagination);
       } catch (error) {
         console.log('FAIL TO FETCH POST LIST: ', error.message);
       }
     }
 
     fetchPostList();
-  }, [])
+  }, [filters]);
 
-  function handleTodoList(todo) {
-    // console.log(todo);
-    const index = todoList.findIndex(x => x.id === todo.id);
-    if(index < 0) return;
+  const handlePageChange = (newPage) => {
+    // console.log('New page: ', newPage);
+    setFilters({
+      ...filters,
+      _page: newPage,
+    });
+  };
+
+  // function handleTodoList(todo) {
+  //   // console.log(todo);
+  //   const index = todoList.findIndex(x => x.id === todo.id);
+  //   if(index < 0) return;
     
-    const newTodoList = [...todoList];
-    newTodoList.splice(index, 1);
-    setTodoList(newTodoList);
-  }
+  //   const newTodoList = [...todoList];
+  //   newTodoList.splice(index, 1);
+  //   setTodoList(newTodoList);
+  // }
 
   // function handleTodoFormSubmit(formValues) {
   //   // console.log(formValues);
@@ -77,6 +99,10 @@ function App() {
       <TodoList todos={todoList} onTodoClick={handleTodoList}/> */}
 
       <PostList posts={postList} />
+      <Pagination 
+        pagination={pagination}
+        onPageChange={handlePageChange} 
+      />
     </div>
   );
 }
---
+++ src/components/Pagination/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Pagination = ({ pagination = {}, onPageChange = null }) => {
+    const { _page, _limit, _totalRows } = pagination;
+    const totalPages = Math.ceil(_totalRows / _limit);
+
+    const handlePageChange = (newPage) => {
+        if (onPageChange) {
+            onPageChange(newPage);
+        }
+    }
+
+    return (
+        <div>
+            <button
+                disabled={_page <= 1}
+                onClick={() => handlePageChange(_page - 1)}
+            >
+                Prev
+            </button>
+
+            <button
+                disabled={_page >= totalPages}
+                onClick={() => handlePageChange(_page + 1)}
+            >
+                Next
+            </button>
+        </div>
+    );
+}
+
+export default Pagination;
